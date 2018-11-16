206d2fbf33ef1f70ecf41facba3cbd53f43d0bc2
Remco <dewilderemco13@gmail.com>
2018-11-16 14:25:18 +0100

    .

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 console.log('[app.js] Started');
 const fs = require('fs');
+const os = require('os');
+const lo = require('lodash');
+const notes = require('./notes.js');
 
+console.log(notes);
+
+let user = os.userInfo();
 try {
-    fs.appendFileSync('statements.txt', 'const / let');
+    fs.appendFileSync('statements.txt', 'hello '+ user.username );
+    console.log('[app.js] file was appended');
 }
 catch (e) {
     console.log('[ERROR]:', e);
 }
 
-console.log('[app.js] file was appended');
+
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('[notes.js] Started');
+
+module.exports.addNote = () => {
+    console.log('Add note.');
+    return 'New note';
+};
